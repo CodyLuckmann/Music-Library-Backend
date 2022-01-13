92c28088a3c9dd41c2182399dfc5da705700a8fb
CodyLuckmann <cluckmann@live.com>
2022-01-13 10:12:56 -0600

    fixed issue with url paths

--- musiclibrary/music/urls.py
+++ musiclibrary/music/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('127.0.0.1:8000/music/', views.SongList.as_view()),
-    path('127.0.0.1:8000/music/<int:pk>/', views.SongDeatil.as_view()),
+    path('music/', views.SongList.as_view()),
+    path('music/<int:pk>/', views.SongDeatil.as_view()),
 ]
