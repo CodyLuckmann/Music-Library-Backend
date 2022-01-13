109b9ddf84e1fd23857a44e0abaa2986b96299c8
CodyLuckmann <cluckmann@live.com>
2022-01-13 09:48:03 -0600

    added path for detail view

--- musiclibrary/music/urls.py
+++ musiclibrary/music/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('music/', views.SongList.as_view()),
+    path('127.0.0.1:8000/music/', views.SongList.as_view()),
+    path('127.0.0.1:8000/music/<int:pk>/', views.SongDeatil.as_view()),
 ]
