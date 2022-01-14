8f838fa1a627e46ab1404808417a8d2e42dbe8e9
CodyLuckmann <cluckmann@live.com>
2022-01-14 09:32:38 -0600

    added cors headers

--- musiclibrary/musiclibrary/settings.py
+++ musiclibrary/musiclibrary/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'music',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+CORS_ORIGN_ALLOW_ALL=True
 
 ROOT_URLCONF = 'musiclibrary.urls'
 
